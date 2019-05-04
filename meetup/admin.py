bcfc00572daa84b4b574e667ec38795a96694043
Ayush Kohli <ayushk96@hotmail.com>
2019-05-03 23:24:59 -0500

    CRUD functionality for admin

--- meetup/admin.py
+++ meetup/admin.py
@@ -1,6 +1,24 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import *
 
-# Register your models here.
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    max_num = 1
+    can_delete = False
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [ProfileInline]
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2','username','first_name','last_name'),}),)
+       
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
+admin.site.register(Location)
