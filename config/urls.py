48714ba5b36ed88eb774ea07ea929763f78d08ce
Ayush Kohli <ayushk96@hotmail.com>
2019-05-05 03:12:27 -0500

    incremental commit. starting notifications

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^meetup/',include('meetup.urls')),
+    url(r'^ meetup/notifications', include('notify.urls', 'notifications')),
 ]
