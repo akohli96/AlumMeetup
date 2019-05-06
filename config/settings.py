84bb1e59748bc64b16a2d67e7630d459cae360f5
Ayush Kohli <ayushk96@hotmail.com>
2019-05-05 23:01:58 -0500

    Get rid of crispy form

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,6 @@ LOGOUT_REDIRECT_URL = 'login'
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
